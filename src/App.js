13f65764d048d96176b66c5027cfe11ce30e8586
binayakdas <20cs3023@rgipt.ac.in>
2024-08-15 20:31:02 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import {useEffect, useState} from "react"
-import './App.css';
-import './style.scss';
-import './media-query.css';
-import Home from './pages/Home';
-import {Routes, Route, useNavigate, Navigate} from "react-router-dom"
-import { ToastContainer } from 'react-toastify';
+import { useState, useEffect } from "react";
+import "./App.css";
+import "./style.scss";
+import "./media-query.css";
+import Home from "./pages/Home";
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Detail from './pages/Detail';
-import AddEditBlog from './pages/AddEditBlog';
-import About from './pages/About';
-import NotFound from './pages/NotFound';
+import Detail from "./pages/Detail";
+import AddEditBlog from "./pages/AddEditBlog";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 import Header from "./components/Header";
 import Auth from "./pages/Auth";
 import { auth } from "./firebase";
 import { signOut } from "firebase/auth";
-
+import TagBlog from "./pages/TagBlog";
+import CategoryBlog from "./pages/CategoryBlog";
+import ScrollToTop from "./components/ScrollToTop";
+import Blogs from "./pages/Blogs";
 
 function App() {
   const [active, setActive] = useState("home");
   const [user, setUser] = useState(null);
 
-  const navigate = useNavigate;
+  const navigate = useNavigate();
 
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
-      if(authUser){
+      if (authUser) {
         setUser(authUser);
-      }
-      else{
+      } else {
         setUser(null);
       }
     });
@@ -43,26 +45,52 @@ function App() {
 
   return (
     <div className="App">
-      <Header 
-      setActive={setActive} 
-      active={active} 
-      user={user}
-      handleLogout={handleLogout}
+      <Header
+        setActive={setActive}
+        active={active}
+        user={user}
+        handleLogout={handleLogout}
       />
-      <ToastContainer position="top-center"/>
+      <ScrollToTop />
+      <ToastContainer position="top-center" />
       <Routes>
-        <Route path="/" element ={<Home setActive={setActive} user={user}/>}/>
-        <Route path="/detail/:id" element ={<Detail setActive={setActive}/>}/>
-        <Route path="/create" 
-        element ={user?.uid ? <AddEditBlog user={user}/> : <Navigate to ="/" />}
+        <Route
+          path="/"
+          element={<Home setActive={setActive} active={active} user={user} />}
         />
-        <Route path="/update/:id" 
-        element ={user?.uid ? <AddEditBlog user={user} setActive={setActive}/> : <Navigate to ="/" />}
+        <Route
+          path="/search"
+          element={<Home setActive={setActive} user={user} />}
         />
-        <Route path="/about" element ={<About />}/>
-        <Route path="/auth" element ={<Auth setActive={setActive} setUser={setUser
-        }/>}/>
-        <Route path="*" element ={<NotFound />}/>
+        <Route
+          path="/detail/:id"
+          element={<Detail setActive={setActive} user={user} />}
+        />
+        <Route
+          path="/create"
+          element={
+            user?.uid ? <AddEditBlog user={user} /> : <Navigate to="/" />
+          }
+        />
+        <Route
+          path="/update/:id"
+          element={
+            user?.uid ? (
+              <AddEditBlog user={user} setActive={setActive} />
+            ) : (
+              <Navigate to="/" />
+            )
+          }
+        />
+        <Route path="/blogs" element={<Blogs setActive={setActive} />} />
+        <Route path="/tag/:tag" element={<TagBlog setActive={setActive} />} />
+        <Route path="/category/:category" element={<CategoryBlog setActive={setActive}  />} />
+        <Route path="/about" element={<About />} />
+        <Route
+          path="/auth"
+          element={<Auth setActive={setActive} setUser={setUser} />}
+        />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
-import {initializeApp} from "firebase/app";
-import {getFirestore} from "firebase/firestore";
-import {getStorage} from "firebase/storage";
-import {getAuth} from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDuQnR0jzw_e-0dc0YTDoWyWkWaeEdQPoQ",
+  apiKey: "AIzaSyDuQnR0jzw_e-0dc0YTDoWyWkWaeEdQPoQ",
     authDomain: "progblog-ae49c.firebaseapp.com",
     projectId: "progblog-ae49c",
     storageBucket: "progblog-ae49c.appspot.com",
     messagingSenderId: "139481202837",
     appId: "1:139481202837:web:0dc1d041f95870bd755b9c"
-  };
+};
 
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const db = getFirestore(app);
-  const storage = getStorage(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-  export {auth, db, storage};
+export { auth, db, storage };
